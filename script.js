705ecd11fb4126bce35335cee186393118ec2860
milkhpbd106 <morgianna333@gmail.com>
2025-05-25 02:32:50 +0700

    Update script.js

--- script.js
+++ script.js
@@ -25,10 +25,10 @@ const messages2 = [
 ];
 
 // Kiểm tra ngày
-function isToday106() {
-  const today = new Date();
+//function isToday106() {
+//  const today = new Date();
 //  return today.getMonth() === 5 && today.getDate() === 10; // Tháng 6 là số 5
-  today.setMonth(5);  // Tháng 6 (vì tháng đếm từ 0)
+//  today.setMonth(5);  // Tháng 6 (vì tháng đếm từ 0)
   // Ngày 10
 
 //}
